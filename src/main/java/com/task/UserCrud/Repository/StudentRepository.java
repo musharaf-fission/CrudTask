a36bdb40aa4b4bfd45b786a7d18639ccd35cce42
musharaf-fission <musharaf.ahmed@fissionlabs.com>
2022-05-12 16:27:11 +0530

    task

--- src/main/java/com/task/UserCrud/Repository/StudentRepository.java
+++ src/main/java/com/task/UserCrud/Repository/StudentRepository.java
@@ -1,9 +1,11 @@
 package com.task.UserCrud.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.task.UserCrud.Model.Student;
 
+@RepositoryRestResource(path = "boy")
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
 }
